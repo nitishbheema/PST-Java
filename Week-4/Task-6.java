fc4003cf268b43fb1d8590a8141ecc5d5a5c8f4f
Bheema Nitish <nitishbheema@gmail.com>
2026-02-10 09:53:39 +0530

    Update Task-6.java

--- Week-4/Task-6.java
+++ Week-4/Task-6.java
@@ -1,18 +1,18 @@
-public static void moveZeroes(int[] nums) {
-    int i = 0;
+class Solution {
+    public void moveZeroes(int[] nums) {
 
-    if (nums.length == 0 || nums.length == 1) {
-        return;
-    }
+        int pos = 0; // position for next non-zero
 
-    for (int j = 0; j < nums.length; j++) {
-        if (nums[j] != 0) {
-            nums[i] = nums[j];
-            i++;
+        // move non-zero elements forward
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[pos++] = nums[i];
+            }
         }
-    }
 
-    for (int index = i; index < nums.length; index++) {
-        nums[index] = 0;
+        // fill remaining positions with zeros
+        while (pos < nums.length) {
+            nums[pos++] = 0;
+        }
     }
 }
